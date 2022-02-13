655c77f8ee0da067b37b4dea60a4905b85c38614
marvo <trujillomarvin@hotmail.com>
2022-02-13 00:14:41 -0800

    localStorage issues solved

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ submit.addEventListener('click', spitName);
 
 
 window.localStorage.setItem('firstName', document.querySelector("input[name='first_name'").value);
-
-// window.localStorage.setItem('firstName', document.querySelector("input[name='first_name'").value);
-// window.localStorage.setItem('firstName', document.querySelector("input[name='first_name'").value);
-// window.localStorage.setItem('firstName', document.querySelector("input[name='first_name'").value);
-// window.localStorage.setItem('firstName', document.querySelector("input[name='first_name'").value);
+window.localStorage.setItem('lastName', document.querySelector("input[name='last_name'").value);
+window.localStorage.setItem('email', document.querySelector("input[name='email'").value);
+window.localStorage.setItem('phoneNum', document.querySelector("input[name='phoneNum'").value);
+window.localStorage.setItem('password', document.querySelector("input[name='password'").value);
 
 
--- loveu.js
+++ loveu.js
@@ -1,6 +1,9 @@
 function getValues() {
     firstName = window.localStorage.getItem('firstName');
-
+    lastName = window.localStorage.getItem('lastName');
+    email = window.localStorage.getItem('email');
+    phoneNum = window.localStorage.getItem('phoneNum');
+    password = window.localStorage.getItem('password');
 }
 
 getValues();
